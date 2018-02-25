505b12cd62bc1650980343e8abdf2e31ea721ab0
Ricky Joudrey <rjoudrey@mail.usf.edu>
2018-02-25 03:52:44 -0800

    Do week 4 project

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -12,3 +12,65 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+
+(START)
+// offset = 0
+   @offset
+   M=0
+   
+// ptr = SCREEN
+   @SCREEN
+   D=A
+   
+   @ptr
+   M=D
+   
+// if kbd == 0, jmp to clear
+   @KBD
+   D=M
+   
+   @CLEAR
+   D;JEQ
+   
+// set color to -1
+   @color
+   M=-1
+   
+   @CHECK_LOOP_CONDITION
+   0;JMP
+
+(CLEAR)
+// set color to 0
+   @color
+   M=0
+
+(CHECK_LOOP_CONDITION)
+// if offset == screen_len, jmp to start
+   @offset
+   D=M
+   
+   @8192
+   D=D-A
+   
+   @START
+   D;JEQ
+
+// *ptr = color
+   @color
+   D=M
+
+   @ptr
+   A=M
+   M=D
+
+// increment ptr and offset
+   @offset
+   M=M+1
+
+   @ptr
+   M=M+1
+
+// go back to check loop condition
+   @CHECK_LOOP_CONDITION
+   0;JMP
+
--- 04/mult/mult.asm
+++ 04/mult/mult.asm
@@ -7,3 +7,31 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+
+// sum = 0
+   @R2
+   M=0
+// i = 0
+   @i
+   M=0
+(CHECK_LOOP_CONDITION)
+// if i == a, then jump to end
+   @R0
+   D=M
+   @i
+   D=D-M
+   @END
+   D;JEQ
+// loop body (sum = sum + b)
+   @R1
+   D=M
+   @R2
+   M=D+M
+// ++i
+   @i
+   M=M+1
+   @CHECK_LOOP_CONDITION
+   0;JMP
+(END)
+   @END
+   0;JMP
